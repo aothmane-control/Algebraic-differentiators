9ee723a444ddfc9c3956798729da5a4d9fbb0c2b
Amine Othmane <amine.othmane@uni-saarland.de>
2024-01-01 14:32:26 +0100

    Added new release name

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "docs/source/usage.rst").read_text()
 setup(
   name = 'AlgDiff',
   packages = ['AlgDiff'],
-  version = '2.2.1',
+  version = '2.2.1.1',
   license='bsd-3-clause',
   description = 'AlgDiff is a Python class implementing all necessary tools for the design, analysis, and discretization of algebraic differentiators. An interface to Matlab is also provided.',
   
@@ -17,7 +17,7 @@ setup(
   author = 'Amine Othmane',
   author_email = 'amine.othmane@uni-saarland.de',
   url = 'https://github.com/aothmane-control/Algebraic-differentiators',
-  download_url = 'https://github.com/aothmane-control/Algebraic-differentiators/releases/tag/v2.2.1',
+  download_url = 'https://github.com/aothmane-control/Algebraic-differentiators/releases/tag/v2.2.1.1',
   keywords = ['numerical-differentiation ', 'fir-filters', 'orthogonal-polynomials', 'numerical-methods '],
   install_requires=[
           'scipy',
